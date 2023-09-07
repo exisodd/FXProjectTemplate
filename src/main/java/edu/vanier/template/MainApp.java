907f08e182f9f452d1574fb9c39e9ab7f253980a
Ethan Tran <2270000@edu.vaniercollege.qc.ca>
2023-09-07 09:38:41 -0400

    everything

--- src/main/java/edu/vanier/template/MainApp.java
+++ src/main/java/edu/vanier/template/MainApp.java
@@ -1,6 +1,9 @@
 package edu.vanier.template;
 
- 
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * This is a JavaFX project template to be used for creating GUI applications.
  * JavaFX 20.0.2 is already linked to this project in the build.gradle file.
@@ -8,9 +11,14 @@ package edu.vanier.template;
  * @see: Build Scripts/build.gradle
  * @author Sleiman Rabah.
  */
-public class MainApp {
+public class MainApp extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello there!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
