cbd65a6f4b48011a7a9f5e924269a457f8088de7
exisodd <ethantran004@yahoo.ca>
2023-09-10 23:29:10 -0400

    added buttons

--- src/main/java/edu/vanier/template/MainApp.java
+++ src/main/java/edu/vanier/template/MainApp.java
@@ -2,14 +2,11 @@ package edu.vanier.template;
 
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
 
 /**
  * This is a JavaFX project template to be used for creating GUI applications.
@@ -22,16 +19,22 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button button1 = new Button("Button 1");
-        Button button2 = new Button("Button 2");
+        Button button1 = new Button("Open new window");
+        Button button2 = new Button("Add button");
         Button button3 = new Button("Button 3");
+
+        HBox root = new HBox(button1, button2, button3);
+        root.setAlignment(Pos.CENTER);
 
         button1.setOnAction(e -> {
             PopupWindow popupWindow = new PopupWindow();
             popupWindow.show();
         });
 
-        HBox root = new HBox(button1, button2, button3);
+        button2.setOnAction(e -> {
+            root.getChildren().add(new Button("new button"));
+        });
+
         Scene scene = new Scene(root, 500, 500);
 
         primaryStage.setTitle("Title bar");
